fa46e1f5edf3872aaeb6b45718ce298d8404f67a
anh.do <tuananh9h@gmail.com>
2024-07-26 09:34:47 +0700

    local jwt

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import { Request } from 'express';
+import { AuthService } from './auth.service';
+import { AuthPayLoadDto } from './dto/auth.dto';
+import { LocalGuard } from './guards/local.guard';
+import { JwtAuthGuard } from './guards/jwt.guard';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+  // @Post('login')
+  // @UseGuards(LocalGuard)
+  // login(@Body() authPayload: AuthPayLoadDto) {
+  //   const user = this.authService.validateUser(authPayload);
+  //   // if (!user) throw new HttpException('Invalid Credentials', 401);
+  //   return user;
+  // }
+  @Post('login')
+  @UseGuards(LocalGuard)
+  login(@Req() req: Request) {
+    return req.user;
+  }
+
+  @Get('status')
+  @UseGuards(JwtAuthGuard)
+  status(@Req() req: Request) {
+    console.log('Inside AuthController status method');
+    console.log(req.user);
+    return req.user;
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
+import { AuthPayLoadDto } from './dto/auth.dto';
+import { JwtService } from '@nestjs/jwt';
 
+const fakeUsers = [
+  {
+    id: 1,
+    username: 'anson',
+    password: 'password',
+  },
+  {
+    id: 2,
+    username: 'jack',
+    password: 'password123',
+  },
+];
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private jwtService: JwtService) {}
+  validateUser({ username, password }: AuthPayLoadDto) {
+    const findUser = fakeUsers.find((user) => user.username === username);
+    if (!findUser) return null;
+    if (password === findUser.password) {
+      const { password, ...user } = findUser;
+      return this.jwtService.sign(user);
+    }
+  }
+}
